e6613a3775cbd5c82be4294ce39599d56dfec500
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-10-28 22:27:18 +0900

    implement shuffle func

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,10 @@
 package main
 
 func main() {
-	//cards := newDeck()
-	//cards.saveToFile("my_cards")
-
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
+
+	//cards.saveToFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
 }
