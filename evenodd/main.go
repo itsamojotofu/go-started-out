cf8049374e58d418575fbb0f46e0fa41dd35af6e
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-10-29 01:36:32 +0900

    add stupid code

---
+++ evenodd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type ten []int
+
+func main() {
+	t := ten{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, i := range t {
+		if i%2 != 0 {
+			fmt.Println("odd")
+		} else {
+			fmt.Println("even")
+		}
+	}
+}
