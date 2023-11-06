e64588443b724d10efae819b5895f8e8d08ac255
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-11-06 14:27:11 +0900

    Update main.go

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
 func main() {
-	 
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	alex.contactInfo = contactInfo{"alex@gmail.com", 12345}
+
+	alex.updateName("Alexander")
+	alex.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
