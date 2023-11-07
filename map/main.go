72f1b1043fb29c1cf779bd908efe7d6e0720dbfb
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-11-07 17:28:12 +0900

    try map-specific func

--- map/main.go
+++ map/main.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
+	//var colors map[string]string
+
+	colors := make(map[string]string)
 
 	//colors := map[string]string{
 	//	"red":   "#ff0000",
 	//	"green": "#dd2402",
 	//	"blue":  "#ld1122",
 	//}
+
+	colors["red"] = "#ff0000"
+	colors["green"] = "#dd2402"
+
+	delete(colors, "green")
 
 	fmt.Println(colors)
 }
