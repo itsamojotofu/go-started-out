8712cdf24bfb72aac095d8a36eb4b2fd34e812cb
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-11-07 16:36:47 +0900

    map colors

--- map/main.go
+++ map/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var colors map[string]string
+
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#dd2402",
+	//	"blue":  "#ld1122",
+	//}
+
+	fmt.Println(colors)
 }
