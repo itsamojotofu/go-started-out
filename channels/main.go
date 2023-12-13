43c1f1509e67a48c3c4b77dd19147d8d5c753510
sota <112251582+itsamojotofu@users.noreply.github.com>
2023-12-14 00:07:30 +0900

    concurrent routines

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, l := range links {
-		go checkLink(l)
+		go checkLink(l, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
